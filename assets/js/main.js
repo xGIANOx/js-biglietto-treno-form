123b2e626edd45d9559c6a41ee875956801b46eb
xGIANOx <studioenricogian@gmail.com>
2023-03-09 17:54:54 +0100

    [add] lower part

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,33 @@ function userInput () {
     console.log(userKm)
 
     let userAge = document.getElementById("userAge").value;
-    console.log(userAge)
+    document.getElementById('ticket').innerHTML = "Biglietto " + userAge;
+
+    document.getElementById('carriage').innerHTML = Math.floor(Math.random() * 10) + 1;
+
+    document.getElementById('cpCode').innerHTML = Math.floor(Math.random()*90000) + 10000;
+
+    let kmPrice = 0.21
+    let normalPrice = userKm * kmPrice
+
+    let discountTwenty = (normalPrice / 100) * 20;
+    let finalPrice_twenty = normalPrice - discountTwenty;
+    console.log(finalPrice_twenty)
+
+
+    let discountFourty = (normalPrice / 100) * 40;
+    let finalPrice_fourty = normalPrice - discountFourty;
+
+
+    if (userAge == "Minorenne"){
+        document.getElementById('ticketPrice').innerHTML = finalPrice_twenty + " €";
+    } else if (userAge == "Over 65"){
+        document.getElementById('ticketPrice').innerHTML = finalPrice_fourty + " €";
+    } else{
+        document.getElementById('ticketPrice').innerHTML = normalPrice + " €";
+
+    }
+    
 
 }
 
